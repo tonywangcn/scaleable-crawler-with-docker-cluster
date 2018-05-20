60c73a8b8f537354daa21594cf8e5468edd53fc0
TonyWangBit <plantpark.net@gmail.com>
2018-05-21 04:56:34 +0800

    update docker inner network link

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 from celery import Celery
-app = Celery('test_celery',broker='amqp://admin:mypass@10.1.1.234:5673',backend='rpc://',include=['test_celery.tasks'])
+app = Celery('test_celery',broker='amqp://admin:mypass@rabbit:5672',backend='rpc://',include=['test_celery.tasks'])
 
